b07914627dfaff1c7165a7a408f7cfe43d01f535
Traknight <traknight@outlook.com>
2021-09-12 11:32:31 -0400

    Created GUI Window

--- .idea/GUI.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-
-class Window():
-    print("GUI Initializing ")
-    def __init__(self):
-        #Window Title, size and bitmap
-        master.title('MSUS')
-        master.geometry('500x600')
-        master.iconbitmap('img/OIP .ico')
